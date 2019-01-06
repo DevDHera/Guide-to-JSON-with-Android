6f11ffcef15733b220e6f7cd9c2728a902e89959
Devin Herath <devdhera@gmail.com>
2019-01-06 12:33:27 +0530

    Configured MainActivity

---
+++ app/src/main/java/com/devin/jsonparsing/MainActivity.java
@@ -0,0 +1,187 @@
+package com.devin.jsonparsing;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.yarolegovich.lovelydialog.LovelyStandardDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressDialog pro;
+    private ListView lv;
+
+    private static String url = "https://api.androidhive.info/contacts/";
+
+    ArrayList<HashMap<String, String>> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<>();
+        lv = findViewById(R.id.list);
+
+        new GetContacts().execute();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                HashMap<String, String> hashMap = (HashMap<String, String>) lv.getAdapter().getItem(i);
+
+                new LovelyStandardDialog(MainActivity.this, LovelyStandardDialog.ButtonLayout.VERTICAL)
+                        .setTopColorRes(R.color.colorPrimary)
+                        .setButtonsColorRes(R.color.darkDeepOrange)
+                        .setIcon(R.drawable.ic_info_outline_white_36dp)
+                        .setTitle("Full Details")
+                        .setMessage(
+                                "ID: \t\t\t\t\t\t\t\t"+hashMap.get("id")+"\n"
+                                +"Name: \t\t\t\t"+hashMap.get("name")+"\n"
+                                +"Email: \t\t\t\t"+hashMap.get("email")+"\n"
+                                +"Address: \t\t"+hashMap.get("address")+"\n"
+                                +"Gender: \t\t\t"+hashMap.get("gender")+"\n"
+                                +"Mobile: \t\t\t"+hashMap.get("mobile")+"\n"
+                                +"Home: \t\t\t\t"+hashMap.get("home")+"\n"
+                                +"Office: \t\t\t\t"+hashMap.get("office")+"\n")
+
+                        .setPositiveButton(android.R.string.ok, null)
+                        .show();
+            }
+        });
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pro = new ProgressDialog(MainActivity.this);
+            pro.setMessage("Please wait...");
+            pro.setCancelable(false);
+            pro.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("address", address);
+                        contact.put("gender", gender);
+                        contact.put("mobile", mobile);
+                        contact.put("home", home);
+                        contact.put("office", office);
+
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pro.isShowing())
+                pro.dismiss();
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{"name", "email",
+                    "mobile"}, new int[]{R.id.name,
+                    R.id.email, R.id.mobile});
+
+            lv.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_fetch){
+            new GetContacts().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
